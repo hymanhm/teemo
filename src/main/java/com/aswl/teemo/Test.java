7c6238e24dc8cb42c32859084bf5f3bba111f886
huming <huming@111.com.cn>
2017-07-11 18:40:11 +0800

    test

---
+++ src/main/java/com/aswl/teemo/Test.java
@@ -0,0 +1,10 @@
+package com.aswl.teemo;
+
+/**
+ * Created by huming on 2017/7/11.
+ */
+public class Test {
+    public static  void main(String[] avg){
+        System.out.println("hello word");
+    }
+}
